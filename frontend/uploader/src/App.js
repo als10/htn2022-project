25354d41b25a2741a9fcb8d9fe5bf19bff73dd09
Edward Wang <edwardw483@gmail.com>
2022-09-17 14:05:44 -0400

    page routing

--- frontend/uploader/src/App.js
+++ frontend/uploader/src/App.js
@@ -1,11 +1,40 @@
 import './App.css';
 import React from 'react';
-import Uploader from './components/uploader'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import UploadPage from './pages/UploadPage'
+import Home from './pages/Home'
 
 const App = () => (
-  <div>
-    <Uploader />
-  </div>
+  <Router>
+      <div>
+        {/* <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav> */}
+        <Switch>
+          <Route path="/upload">
+            <UploadPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
 );
 
 export default App;
---
+++ frontend/uploader/src/pages/UploadPage.js
@@ -0,0 +1,12 @@
+import '../App.css';
+import React from 'react';
+import Uploader from '../components/uploader'
+
+const App = () => ( 
+  <div>
+    <div>Upload your audio/video file.</div>
+    <Uploader />
+  </div>
+);
+
+export default App;
