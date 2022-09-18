7570ccd69dce2993c16117c1f56776215604b066
Alston D'mello <31090569+als10@users.noreply.github.com>
2022-09-17 23:37:23 -0400

    move loader to separate component

--- frontend/uploader/src/pages/UploadPage.js
+++ frontend/uploader/src/pages/UploadPage.js
@@ -2,25 +2,12 @@ import '../App.css';
 import React, { useState } from 'react';
 import UploadButton from '../components/UploadButton'
 import { Typography } from 'antd';
-import { Waveform } from '@uiball/loaders';
 import SummaryPage from './SummaryPage';
+import Loader from '../components/Loader';
 
 const UploadPage = () => {
   const [loaded, setLoaded] = useState(true)
   const [data, setData] = useState(null)
-
-  if (!loaded) {
-    return (
-      <div style={{ width: '50%', display: 'flex',  justifyContent:'center', alignItems:'center' }}>
-        <Waveform 
-          size={72}
-          lineWeight={3.5}
-          speed={1} 
-          color="black" 
-        />
-      </div>
-    )
-  } 
 
   if (data) return <SummaryPage data={data} />
 
@@ -29,7 +16,11 @@ const UploadPage = () => {
       <Typography.Title level={1} style={{ width: '50%' }}>
         Upload the audio from your lecture
       </Typography.Title>
-      <UploadButton setLoaded={setLoaded} setData={setData} />
+      <div style={{ width: '50%', display: 'flex',  justifyContent:'center', alignItems:'center' }}>
+        {loaded
+          ? <UploadButton setLoaded={setLoaded} setData={setData} />
+          : <Loader />}
+      </div>
     </div>
   )
 }
